47ac43725c15bedde6a0afa80acadf52b50fd1ed
Tav Hafner <23143092+haftav@users.noreply.github.com>
2018-11-14 19:00:46 -0700

    first commit

---
+++ src/views/Waterfall.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Waterfall extends Component {
+    state = {
+        images: []
+    }
+    componentDidMount() {
+        axios.get(`${process.env.REACT_APP_PIXABAY_API_URL}?key=${process.env.REACT_APP_PIXABAY_API_KEY}`).then(res => {
+            let newImages = res.data.hits;
+            console.log(newImages)
+            this.setState({
+                images: newImages
+            });
+        })
+    }
+
+    render() {
+        return(
+            <div className="Waterfall">
+                {
+                    this.state.images.map(image => {
+                        return (
+                            <div className="image-wrapper">
+                                <img src={image.largeImageURL}/>
+                            </div>
+
+                        )
+                    })
+                }
+
+            </div>
+        )
+    }
+}

5d54d33ee38b71ad7908a1eeb2e8d043f0038eef
Tav Hafner <23143092+haftav@users.noreply.github.com>
2018-12-04 22:13:09 -0700

    adding in search functionality

--- src/views/Waterfall.js
+++ src/views/Waterfall.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import SearchBar from './Waterfall/SearchBar';
+
 import axios from 'axios';
 
 export default class Waterfall extends Component {
     state = {
-        images: []
+        images: [],
+        searchTerm: ''
     }
     componentDidMount() {
         axios.get(`${process.env.REACT_APP_PIXABAY_API_URL}?key=${process.env.REACT_APP_PIXABAY_API_KEY}`).then(res => {
@@ -14,10 +17,34 @@ export default class Waterfall extends Component {
         })
     }
 
+    handleInputChange = (e) => {
+        console.log(e.target.value);
+        let newTerm = e.target.value;
+        this.setState({
+            searchTerm: newTerm
+        })
+    }
+
+    handleClick = () => {
+        if (this.state.searchTerm) {
+            axios.get(`${process.env.REACT_APP_PIXABAY_API_URL}?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${this.state.searchTerm}`).then(res => {
+                let newImages = res.data.hits;
+                this.setState({
+                    images: newImages
+                });
+            });
+        } else {
+            alert("Please enter a search term.");
+        }
+    }
+
     render() {
         return(
             <div className="Waterfall">
-                <div className="Waterfall_spotlight"></div>
+                <div className="Waterfall_spotlight">
+                    <SearchBar handleInputChange={this.handleInputChange} value={this.state.searchTerm}/>
+                    <button onClick={this.handleClick}>Get Images</button>
+                </div>
                 {
                     this.state.images.map(image => {
                         return (

af8cc11bb1489759a5d8cd3bfd8bc91072a1784a
Tav Hafner <23143092+haftav@users.noreply.github.com>
2018-12-13 19:44:13 -0700

    moved preloading logic to HOC

--- src/components/ComponentWithSubscription.js
+++ src/components/ComponentWithSubscription.js
@@ -5,45 +5,66 @@ const ComponentWithSubscription = (ComponentToRender) => {
     return class extends Component {
         state = {
             images: [],
-            status: 'PENDING'
+            loadedImages: [],
+            loading: true
         }
         componentDidMount() {
             axios.get(`${process.env.REACT_APP_PIXABAY_API_URL}?key=${process.env.REACT_APP_PIXABAY_API_KEY}`).then(res => {
                 let newImages = res.data.hits;
                 this.setState({
                     images: newImages,
-                    status: 'COMPLETE'
-                });
+                }, () => this.preloadImages(newImages));
             }).catch((err) => {
                 console.error(err);
+            })
+        }
+
+        handleLoad = (imageSrc) => {
+            let newLoadedImages = [...this.state.loadedImages];
+            newLoadedImages.push(imageSrc);
+            if (newLoadedImages.length === this.state.images.length) {
                 this.setState({
-                    status: 'ERROR'
+                    loadedImages: newLoadedImages,
+                    loading: false
                 })
+            } else {
+                this.setState({
+                    loadedImages: newLoadedImages
+                })
+            }
+        }
+
+        preloadImages = (images) => {
+            images.forEach((image) => {
+                const img = new Image();
+                img.onload = () => this.handleLoad(image.largeImageURL)
+                img.src = image.largeImageURL
             })
         }
 
         handleClick = (searchTerm) => {
-                console.log(searchTerm);
-                axios.get(`${process.env.REACT_APP_PIXABAY_API_URL}?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${searchTerm}`).then(res => {
-                    let newImages = res.data.hits;
-                    if (newImages.length === 0) {
-                        throw new Error('No results found for that query.')
-                    }
-                    this.setState({
-                        images: newImages,
-                        status: 'COMPLETE'
-                    });
-                }).catch((e) => {
-                    console.error(e);
-                    this.setState({
-                        images: [],
-                        status: 'ERROR'
-                    })
-                });
+            if (!searchTerm) return alert('Please provide a search term.');
+            axios.get(`${process.env.REACT_APP_PIXABAY_API_URL}?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${searchTerm}`).then(res => {
+                let newImages = res.data.hits;
+                if (newImages.length === 0) throw new Error('No images found.')
+                this.setState({
+                    images: newImages,
+                    loadedImages: [],
+                    loading: true
+                }, () => this.preloadImages(newImages));
+            }).catch((e) => {
+                console.error(e);
+                this.setState({
+                    images: [],
+                    loadedImages: [],
+                    loading: false
+                })
+            });
         }
         render() {
+            console.log(this.state.loadedImages.length, this.state.loading)
             return (
-                <ComponentToRender handleClick={this.handleClick} images={this.state.images} {...this.props} />
+                <ComponentToRender handleClick={this.handleClick} images={this.state.loadedImages} loading={this.state.loading} {...this.props} />
             )
         }
     }
--- src/views/Waterfall.js
+++ src/views/Waterfall.js
@@ -4,18 +4,7 @@ import _isEqual from 'lodash.isequal';
 
 export default class Waterfall extends Component {
     state = {
-        loadedImages: [],
-        searchTerm: '',
-        loading: true
-    }
-
-    componentDidUpdate(prevProps) {
-        //This doesn't update if 2 searches return in a row with no results. May have to move loading logic to HOC
-        if (this.props.images.length === 0) {
-            this.setState({
-                loading: false
-            })
-        }
+        searchTerm: ''
     }
 
     shouldComponentUpdate(nextProps, nextState) {
@@ -33,76 +22,27 @@ export default class Waterfall extends Component {
         })
     }
 
-    handleClick = (searchTerm) => {
-        if (!searchTerm) return alert("Please enter a search term.");
-        if (!Waterfall.searchTerm[searchTerm]) {
-            Waterfall.searchTerm[searchTerm] = searchTerm;
-            this.setState({
-                loading: true,
-                loadedImages: []
-            })
-            this.props.handleClick(searchTerm);
-        } else {
-            this.props.handleClick(searchTerm)
-            this.setState(() => {
-                return {
-                    loading: true,
-                    loadedImages: []
-                }
-            }, () => setTimeout(() => this.setState({ loading: false }), 250))
-        }
-    }
-
-    onLoad = (feedItem) => {
-        let newLoadedImages = [...this.state.loadedImages];
-        newLoadedImages.push(feedItem);
-
-        if (newLoadedImages.length >= this.props.images.length) {
-            this.setState({
-                loading: false,
-                loadedImages: newLoadedImages
-            })
-        } else {
-            this.setState({
-                loadedImages: newLoadedImages
-            })
-        }
-    }
-
     render() {
-        console.log(this.state.loadedImages)
-        let images = this.state.loadedImages.map(image => {
+        let images = this.props.images.map(image => {
             return (
-                <div className="image-wrapper" key={image.largeImageURL}>
-                    <img src={image.largeImageURL} />
+                <div className="image-wrapper" key={image}>
+                    <img src={image} />
                 </div>
             )
-        });
+        })
         return (
             <div className="Waterfall">
                 <div className="Waterfall_spotlight">
-                    <SearchBar handleInputChange={this.handleInputChange} handleClick={this.handleClick} value={this.state.searchTerm} />
+                    <SearchBar handleInputChange={this.handleInputChange} handleClick={this.props.handleClick} value={this.state.searchTerm} />
                 </div>
                 {
-                    (this.state.loading ? <div className="Waterfall_loading"><div className="Waterfall_loading_spinner" /></div> : images)
+                    (this.props.loading ? <div className="Waterfall_loading"><div className="Waterfall_loading_spinner" /></div> : images)
                 }
                 {
-                    this.props.images.length === 0 && !this.state.loading && <div>No images found.</div>
+                    this.props.images.length === 0 && !this.props.loading && <div>No images found.</div>
                 }
-                <div className="Waterfall_hidden">
-                    {
-                        this.props.images.map((image, i) => {
-                            return <img
-                                src={image.largeImageURL}
-                                onLoad={() => this.onLoad(image)}
-                                key={image.largeImageURL} />
-                        })
-                    }
-                </div>
+
             </div>
         )
     }
 }
-
-Waterfall.searchTerm = {};
-
